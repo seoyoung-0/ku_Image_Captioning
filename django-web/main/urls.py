ddf0c21fe96f14d02ca55f5261c040955b01c7c3
Park seoyoung <52600701+seoyoung-0@users.noreply.github.com>
2021-06-01 17:29:13 +0900

    --

--- django-web/main/urls.py
+++ django-web/main/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve
 
 urlpatterns =[
     path('',index,name='index'),
@@ -12,8 +11,4 @@ urlpatterns =[
 ]
 
 # 이미지 URL 설정
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [
-    url(r'^media/(?P<path>.*)',serve, {
-    'document_root': settings.MEDIA_ROOT}),]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
